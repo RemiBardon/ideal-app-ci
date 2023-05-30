b28110d88d7294759a49993ecb0ea0a589f4b90a
Rémi Bardon <remi.bardon.dev@gmail.com>
2023-05-30 15:44:08 +0200

    Initial Commit

---
+++ Chimpanzee/ChimpanzeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChimpanzeeApp.swift
+//  Chimpanzee
+//
+//  Created by Rémi Bardon on 30/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChimpanzeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
